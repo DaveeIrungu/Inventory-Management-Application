4e25699f152a0792acf333cb978872ecb44306d6
developerdavee <developerdavee@gmail.com>
2019-06-18 12:19:12 +0300

    Description of application, input variables and features

--- backend.py
+++ backend.py
@@ -6,9 +6,45 @@ def connect():
     conn = sqlite3.connect("store.db")
     cur = conn.cursor()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY, item_name text, item_manufacturer text, item_serial text, item_year integer)")
+        "CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY, name text, manufacturer text, serial text, year integer)")
     conn.commit()
     conn.close()
 
+
+# Insert function
+def insert(name, manufacturer, serial, year):
+    conn = sqlite3.connect("store.db")
+    cur = conn.cursor()
+    # Null value will autoincrement the primary key
+    cur.execute("INSERT INTO item VALUES (NULL,?,?,?,?)", (name, manufacturer, serial, year))
+    conn.commit()
+    conn.close()
+
+
+# View function
+def view():
+    conn = sqlite3.connect("store.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM item ")
+    rows = cur.fetchall()
+    # Does not require commit method
+    conn.close()
+    return rows
+
+
+# Search function
+# Initialize the parameters with a default empty value to avoid error of no arguments passed
+def search(name="", manufacturer="", serial="", year=""):
+    conn = sqlite3.connect("store.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM item WHERE name=? OR manufacturer=? OR serial=? OR year=?",
+                (name, manufacturer, serial, year))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
 # Call the connect function so that it can always execute on the frontend script
 connect()
+# insert('Transcend', 'China', '00100', 2001)
+print(view())
