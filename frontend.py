2574d520a82bb4394f5068437645e1d80df9f878
developerdavee <developerdavee@gmail.com>
2019-06-25 13:05:18 +0300

    Made frontend.py object oriented Fixed bugs

--- frontend.py
+++ frontend.py
@@ -5,112 +5,123 @@ from backend import Database
 database = Database("store.db")
 
 
-def get_selected_row(event):
-    global selected_tuple
-    # Add [0] to obtain just the 1st item (the index) on the tuple
-    index = list1.curselection()[0]
-    # Obtain all the values on the selected row
-    selected_tuple = list1.get(index)
+class Window:
 
-    # Fill the entry widgets with the selected item using insert function
-    # First empty of existing contents
-    e1.delete(0, END)
-    e1.insert(END, selected_tuple[1])
-    e2.delete(0, END)
-    e2.insert(END, selected_tuple[2])
-    e3.delete(0, END)
-    e3.insert(END, selected_tuple[3])
-    e4.delete(0, END)
-    e4.insert(END, selected_tuple[4])
+    def __init__(self, window):
+        self.window = window
+        self.window.wm_title("Inventory Management Application")
 
+        # Label objects
+        l1 = Label(window, text="Name")
+        l1.grid(row=0, column=0)
+        l2 = Label(window, text="Manufacturer")
+        l2.grid(row=0, column=2)
+        l3 = Label(window, text="Serial")
+        l3.grid(row=1, column=0)
+        l4 = Label(window, text="Year")
+        l4.grid(row=1, column=2)
 
-def view_onClick():
-    # Before executing, first clear the listbox to eliminate redundancy
-    list1.delete(0, END)
-    for row in database.view():
-        # The end argument ensures that every new row is put on a new line in the listbox during iteration
-        list1.insert(END, row)
+        # Entry objects
+        self.name_text = StringVar()
+        self.e1 = Entry(window, textvariable=self.name_text)
+        self.e1.grid(row=0, column=1)
 
+        self.manufacturer_text = StringVar()
+        self.e2 = Entry(window, textvariable=self.manufacturer_text)
+        self.e2.grid(row=0, column=3)
 
-def search_onClick():
-    # Clear the listbox before executing
-    list1.delete(0, END)
-    # .get() captures the input entered in the entry widgets
-    for row in database.search(name_text.get(), manufacturer_text.get(), serial_text.get(), year_text.get()):
-        # Insert all the values in the listbox
-        list1.insert(END, row)
+        self.serial_text = StringVar()
+        self.e3 = Entry(window, textvariable=self.serial_text)
+        self.e3.grid(row=1, column=1)
 
+        self.year_text = StringVar()
+        self.e4 = Entry(window, textvariable=self.year_text)
+        self.e4.grid(row=1, column=3)
 
-def insert_onCLick():
-    list1.delete(0, END)
-    database.insert(name_text.get(), manufacturer_text.get(), serial_text.get(), year_text.get())
-    # Display these values on the listbox
-    list1.insert(END, (name_text.get(), manufacturer_text.get(), serial_text.get(), year_text.get()))
+        # Listbox object
+        self.list1 = Listbox(window, height=6, width=35)
+        self.list1.grid(row=2, column=0, rowspan=6, columnspan=2)
+        # Bind method to bind a function the listbox event, ie. selected row to be deleted/updated
+        self.list1.bind('<<ListboxSelect>>', self.get_selected_row)
 
+        # Scrollbar
+        sb1 = Scrollbar(window)
+        sb1.grid(row=4, column=2, rowspan=6)
 
-def delete_onClick():
-    database.delete(selected_tuple[0])
+        # Configure the listbox and scrollbar
+        self.list1.configure(yscrollcommand=sb1.set)
+        sb1.configure(command=self.list1.yview)
 
+        # Button objects
+        # Careful not to add () at the command function
+        b1 = Button(window, text="Add", width=12, command=self.insert_onCLick)
+        b1.grid(row=2, column=3)
 
-def update_onClick():
-    database.update(selected_tuple[0], name_text.get(), manufacturer_text.get(), serial_text.get(), year_text.get())
+        b2 = Button(window, text="Delete", width=12, command=self.delete_onClick)
+        b2.grid(row=3, column=3)
+
+        b3 = Button(window, text="Update", width=12, command=self.update_onClick)
+        b3.grid(row=4, column=3)
+
+        b4 = Button(window, text="Search", width=12, command=self.search_onClick)
+        b4.grid(row=5, column=3)
+
+        b5 = Button(window, text="View All", width=12, command=self.view_onClick)
+        b5.grid(row=6, column=3)
+
+        b6 = Button(window, text="Close", width=12, command=window.destroy)
+        b6.grid(row=7, column=3)
+
+    def get_selected_row(self, event):
+        global selected_tuple
+        # Add [0] to obtain just the 1st item (the index) on the tuple
+        index = self.list1.curselection()[0]
+        # Obtain all the values on the selected row
+        selected_tuple = self.list1.get(index)
+
+        # Fill the entry widgets with the selected item using insert function
+        # First empty of existing contents
+        self.e1.delete(0, END)
+        self.e1.insert(END, selected_tuple[1])
+        self.e2.delete(0, END)
+        self.e2.insert(END, selected_tuple[2])
+        self.e3.delete(0, END)
+        self.e3.insert(END, selected_tuple[3])
+        self.e4.delete(0, END)
+        self.e4.insert(END, selected_tuple[4])
+
+    def view_onClick(self):
+        # Before executing, first clear the listbox to eliminate redundancy
+        self.list1.delete(0, END)
+        for row in database.view():
+            # The end argument ensures that every new row is put on a new line in the listbox during iteration
+            self.list1.insert(END, row)
+
+    def search_onClick(self):
+        # Clear the listbox before executing
+        self.list1.delete(0, END)
+        # .get() captures the input entered in the entry widgets
+        for row in database.search(self.name_text.get(), self.manufacturer_text.get(), self.serial_text.get(),
+                                   self.year_text.get()):
+            # Insert all the values in the listbox
+            self.list1.insert(END, row)
+
+    def insert_onCLick(self):
+        self.list1.delete(0, END)
+        database.insert(self.name_text.get(), self.manufacturer_text.get(), self.serial_text.get(),
+                        self.year_text.get())
+        # Display these values on the listbox
+        self.list1.insert(END, (
+            self.name_text.get(), self.manufacturer_text.get(), self.serial_text.get(), self.year_text.get()))
+
+    def delete_onClick(self):
+        database.delete(selected_tuple[0])
+
+    def update_onClick(self):
+        database.update(selected_tuple[0], self.name_text.get(), self.manufacturer_text.get(), self.serial_text.get(),
+                        self.year_text.get())
 
 
 window = Tk()
-
-# Name of application on title bar
-window.wm_title("Inventory Management Application")
-
-# Label objects
-l1 = Label(window, text="Name")
-l1.grid(row=0, column=0)
-l2 = Label(window, text="Manufacturer")
-l2.grid(row=0, column=2)
-l3 = Label(window, text="Serial")
-l3.grid(row=1, column=0)
-l4 = Label(window, text="Year")
-l4.grid(row=1, column=2)
-
-# Entry objects
-name_text = StringVar()
-e1 = Entry(window, textvariable=name_text)
-e1.grid(row=0, column=1)
-manufacturer_text = StringVar()
-e2 = Entry(window, textvariable=manufacturer_text)
-e2.grid(row=0, column=3)
-serial_text = StringVar()
-e3 = Entry(window, textvariable=serial_text)
-e3.grid(row=1, column=1)
-year_text = StringVar()
-e4 = Entry(window, textvariable=year_text)
-e4.grid(row=1, column=3)
-
-# Listbox object
-list1 = Listbox(window, height=6, width=35)
-list1.grid(row=2, column=0, rowspan=6, columnspan=2)
-# Bind method to bind a function the listbox event, ie. selected row to be deleted/updated
-list1.bind('<<ListboxSelect>>', get_selected_row)
-# Scrollbar
-sb1 = Scrollbar(window)
-sb1.grid(row=4, column=2, rowspan=6)
-
-# Configure the listbox and scrollbar
-list1.configure(yscrollcommand=sb1.set)
-sb1.configure(command=list1.yview)
-
-# Button objects
-# Careful not to add () at the command function
-b1 = Button(window, text="Add", width=12, command=insert_onCLick)
-b1.grid(row=2, column=3)
-b2 = Button(window, text="Delete", width=12, command=delete_onClick)
-b2.grid(row=3, column=3)
-b3 = Button(window, text="Update", width=12, command=update_onClick)
-b3.grid(row=4, column=3)
-b4 = Button(window, text="Search", width=12, command=search_onClick)
-b4.grid(row=5, column=3)
-b5 = Button(window, text="View All", width=12, command=view_onClick)
-b5.grid(row=6, column=3)
-b6 = Button(window, text="Close", width=12, command=window.destroy)
-b6.grid(row=7, column=3)
-
+Window(window)
 window.mainloop()
